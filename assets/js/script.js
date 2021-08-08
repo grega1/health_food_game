db3f86f98efa0f496e8ef5e51b20cbbd1e4c26c3
unknown <adrianogrego1@gmail.com>
2021-08-08 20:43:23 -0300

    Finalização das funções do jogo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,74 +1,75 @@
 //Imports
 class Game {
   constructor(_name = 'Player1', _score = 0, _currentLevel = 1, _playMusic = true, _soundEffects = true,) {
-      this.name = _name;
-      this.score = _score;
-      this.currentLevel = _currentLevel;
-      this.playMusic = _playMusic;
-      this.soundEffects = _soundEffects;
-      
+    this.name = _name;
+    this.score = _score;
+    this.currentLevel = _currentLevel;
+    this.playMusic = _playMusic;
+    this.soundEffects = _soundEffects;
+
   }
 
   setName(_name) {
-      this.name = _name;
+    this.name = _name;
   }
 
   getName() {
-      return this.name;
+    return this.name;
   }
 
   setScore(_score = 0) {
-      this.score = _score;
+    this.score = _score;
   }
   setCurrentLevel(_currentLevel = 1) {
-      this.currentLevel = _currentLevel;
+    this.currentLevel = _currentLevel;
   }
   getCurrentLevel() {
-      return this.currentLevel;
+    return this.currentLevel;
   }
   getScore() {
-      return this.score;
+    return this.score;
   }
   getIntervalByLevel() {
     switch (this.currentLevel) {
       case 1:
-          return 3000;
-  }};
+        return 3000;
+    }
+  };
   getPointsByLevel() {
     switch (this.currentLevel) {
       case 1:
-          return 10;
-  };
-}
+        return 10;
+    };
+  }
 
 
   //actionPoint = Pontuação recebida por destruir o alvo
   increaseScore(_actionPoint = 10) {
-      return this.score = this.score + parseInt(_actionPoint);
+    return this.score = this.score + parseInt(_actionPoint);
   }
 
   drawSlots(_firstSlot, _lastSlot) {
-      return Math.round(Math.random() * (_lastSlot - _firstSlot)) + _firstSlot;
+    return Math.round(Math.random() * (_lastSlot - _firstSlot)) + _firstSlot;
 
   }
   setPlayMusic(_playMusic) {
-      this.playMusic = _playMusic;
+    this.playMusic = _playMusic;
   }
   setSoundEffects(_soundEffects) {
-      this.soundEffects = _soundEffects;
+    this.soundEffects = _soundEffects;
   }
   getPlayMusic() {
-      return this.playMusic;
+    return this.playMusic;
   }
   getSoundEffects() {
-          return this.soundEffects;
-      }
-      //criar função fora para mutar e executar
-     
+    return this.soundEffects;
+  }
+  //criar função fora para mutar e executar
+
 
   rankingData() {
 
-      return { name: this.name, score: this.score };
+    return { name: this.name, score: this.score };
 
   }
 };
@@ -89,9 +90,9 @@ mainMusic.play();
 mainMusic.loop = true;
 */
 function playAudio(sound) {
-    const audioToPlay = new Audio(sound);
-    audioToPlay.volume = 0.1;
-    audioToPlay.play();
+  const audioToPlay = new Audio(sound);
+  audioToPlay.volume = 0.1;
+  audioToPlay.play();
 }
 
 function gameStart() {
@@ -105,131 +106,125 @@ function gameStart() {
 function finishGame() {
 }
 
-$(document).ready(function() {
-  $("#cta-button").click(()=>{
+$(document).ready(function () {
+  $("#cta-button").click(() => {
     $("#home-game").hide();
     $(".game-painel").show();
+    const audio = new Audio('./audio/game-start.mp4');
+    audio.play();
+    audio.volume = 0.3;
+    audio.play();
+    audio.loop = true;
   })
-  $("#help-icon").click(()=>{
+  $("#help-icon").click(() => {
     $("#container-instructions").show().addClass('absolute-with-z-index');
     $(".game-painel").addClass('relative-with-blur');
   })
-  $("#back-to-game").click(()=>{
+  $("#back-to-game").click(() => {
     $("#container-instructions").hide().removeClass('absolute-with-z-index');
     $(".game-painel").show().removeClass('relative-with-blur');
   })
   gameStart();
   newGame.getScore();
-    $( function() {
-      $( ".column" ).sortable({
-        connectWith: ".column",
-        handle: ".portlet-header",
-        cancel: ".portlet-toggle",
-        placeholder: "portlet-placeholder ui-corner-all"
-      });
-   
-      $( ".portlet" )
-        .addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
-        .find( ".portlet-header" )
-          .addClass( "ui-widget-header ui-corner-all" )
-          .prepend( "<span class='ui-icon ui-icon-minusthick portlet-toggle'></span>");
-   
-      $( ".portlet-toggle" ).on( "click", function() {
-        var icon = $( this );
-        icon.toggleClass( "ui-icon-minusthick ui-icon-plusthick" );
-        icon.closest( ".portlet" ).find( ".portlet-content" ).toggle();
-      });
+  $(function () {
+    $(".column").sortable({
+      connectWith: ".column",
+      handle: ".portlet-header",
+      cancel: ".portlet-toggle",
+      placeholder: "portlet-placeholder ui-corner-all"
     });
-  
-    $( function() {
-      $( ".draggable2" ).draggable({ revert: "valid" });    
-      $( ".draggable" ).draggable({ revert: "invalid" });
-     
-      $( "#droppable" ).droppable({
-        classes: {
-          "ui-droppable-active": "ui-state-active",
-          "ui-droppable-hover": "ui-state-hover"        
-        },
-        drop: function( event, ui ) {
-          let dropValue = newGame.getPointsByLevel(newGame.getCurrentLevel());
-          const classesString = $(ui.draggable).attr('class');
-          const idDragg = "#"+ $(ui.draggable).attr('id')  
-          console.log($(idDragg))    
-          $(idDragg).removeClass("draggable").hide();    
-          if($("#container-foods > div").hasClass("draggable")){
-          if (classesString.includes("draggable2")) {         
+
+    $(".portlet")
+      .addClass("ui-widget ui-widget-content ui-helper-clearfix ui-corner-all")
+      .find(".portlet-header")
+      .addClass("ui-widget-header ui-corner-all")
+      .prepend("<span class='ui-icon ui-icon-minusthick portlet-toggle'></span>");
+
+    $(".portlet-toggle").on("click", function () {
+      var icon = $(this);
+      icon.toggleClass("ui-icon-minusthick ui-icon-plusthick");
+      icon.closest(".portlet").find(".portlet-content").toggle();
+    });
+  });
+
+  $(function () {
+    $(".draggable2").draggable({ revert: "valid" });
+    $(".draggable").draggable({ revert: "invalid" });
+
+    $("#droppable").droppable({
+      classes: {
+        "ui-droppable-active": "ui-state-active",
+        "ui-droppable-hover": "ui-state-hover"
+      },
+      drop: function (event, ui) {
+        let dropValue = newGame.getPointsByLevel(newGame.getCurrentLevel());
+        const classesString = $(ui.draggable).attr('class');
+        const idDragg = "#" + $(ui.draggable).attr('id')
+        $(idDragg).removeClass("draggable").hide();
+        if ($("#container-foods > div").hasClass("draggable")) {
+          if (classesString.includes("draggable2")) {
             const audio = new Audio('./audio/missed.mp4');
-            audio.play(); 
+            audio.play();
             $('#img-hungerChild').css('display', 'none');
-            $('#img-sad-child').css('display', 'flex');    
-            $('#img-happy-child').css('display', 'none');             
-              setTimeout(() => {
-                $('#img-hungerChild').css('display', 'flex');
-                $('#img-sad-child').css('display', 'none'); 
-              },3000)
-          } else {         
+            $('#img-sad-child').css('display', 'flex');
+            $('#img-happy-child').css('display', 'none');
+            setTimeout(() => {
+              $('#img-hungerChild').css('display', 'flex');
+              $('#img-sad-child').css('display', 'none');
+            }, 3000)
+          } else {
             const audio = new Audio('./audio/happy.mp4');
             audio.play();
             $("#img-hungerChild").css('display', 'none');
             $('#img-happy-child').css('display', 'flex');
-            $('#img-sad-child').css('display', 'none'); 
+            $('#img-sad-child').css('display', 'none');
             newGame.increaseScore(dropValue);
-            console.log(dropValue)
-            $("#show-score").text(`${newGame.getScore()}`);
-            console.log(newGame.getScore());
+            $(".show-score").text(`${newGame.getScore()}`);
             setTimeout(() => {
               $('#img-hungerChild').css('display', 'flex');
-              $('#img-happy-child').css('display', 'none'); 
-            },1500)  
-          }}else{
-            setTimeout(() => {
-              $('#img-hungerChild').css('display', 'none');
-              $('#img-happy-child').css('display', 'flex'); 
-            },3000)
-            let userScore = newGame.getScore();
-            let userName = newGame.getName();
-            const userRankData = {
-              name:userName,
-              score:userScore,
-            }
-            $.ajax({
-              url : "http://localhost:3005/ranking",
-              type : 'POST',
-              dataType : 'json',
-              contentType : "application/json",
-              data: JSON.stringify(userRankData), 
-                       
-              }).done(() => {
-           $("#show-score").text(`enviado`);
-         })
-            
+              $('#img-happy-child').css('display', 'none');
+            }, 1500)
           }
-          
-          $( this )       
-            /*.addClass( "ui-state-highlight" )*/
-            .find( "p" )
-              .html( "Dropped!" );
-        }      
-      });
+        } else {
+          setTimeout(() => {
+            $('#img-hungerChild').css('display', 'none');
+            $('#img-happy-child').css('display', 'flex');
+          }, 3000)
+          $("#game-win").show().addClass('absolute-with-z-index');
+          $(".game-painel").addClass('relative-with-blur');
+
+          let userScore = newGame.getScore();
+          let userName = newGame.getName();
+          const userRankData = {
+            name: userName,
+            score: userScore,
+          }
+          $.ajax({
+            url: "http://localhost:3005/ranking",
+            type: 'POST',
+            dataType: 'json',
+            contentType: "application/json",
+            data: JSON.stringify(userRankData),
+
+          }).done(() => {
+            $("#show-score").text(`enviado`);
+          })
+        }
+      }
     });
-    $.ajax({
-      url : "http://localhost:3005/ranking",
-      type : 'GET',
-    }).done((data) => {
-      const dataRanking= JSON.stringify(data);
-      const usersRanking = JSON.parse(dataRanking)
-      usersRanking.forEach((user)=>{
-        $("#ranking").text(`Nome:${user.name} Score:${user.score}`)
-      })
-    
-    });
-    
-  
-    $('#start-game').on('click', function() {
-        console.log('entrei aqui')
-        const audio = new Audio('./audio/game-start.mp4');
-        audio.play(); 
-    });  
-    $
+  });
+  $.ajax({
+    url: "http://localhost:3005/ranking",
+    type: 'GET',
+  }).done((data) => {
+    const dataRanking = JSON.stringify(data);
+    const usersRanking = JSON.parse(dataRanking)
+    usersRanking.forEach((user) => {
+      $("#ranking").text(`Nome:${user.name} Score:${user.score}`)
+    })
+  });
+  //back to home//
+  $("#back-to-home").click(() => {
+    window.location.reload()
+  });
 });
-  
